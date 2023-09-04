5a4e79e01af85b4809af4a0847e11a4aa8d9f90a
cox96de <cox96de@gmail.com>
2023-09-05 01:16:40 +0800

    ci: add arm64 ci runner

--- 01-hello-world/bpf_gen_arm64.go
+++ 01-hello-world/bpf_gen_arm64.go
@@ -1,3 +1,3 @@
 package main
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc ${BPF_CC} -cflags "-O2 -g -Wall -Werror" -target arm64 bpf main.bpf.c -- -I../headers/aarch64 -I../headers
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc ${BPF_CC} -cflags "-O2 -g -Wall -Werror" -target arm64 bpf c/main.bpf.c -- -I../headers/aarch64 -I../headers
